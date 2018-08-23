0e14a83717983006096d3d8e293ec8a32f4c99aa
evgen10 <i.am.mr.jackson96@gmail.com>
2018-08-23 17:32:57 +0600

    first

---
+++ Northwind/Scripts/1/1.1/1.sql
@@ -0,0 +1,7 @@
+﻿--1.	Выбрать в таблице Orders заказы, которые были доставлены после 6 мая 1998 года (колонка ShippedDate) 
+--включительно и которые доставлены с ShipVia >= 2. 
+--Запрос должен возвращать только колонки OrderID, ShippedDate и ShipVia. 
+
+SELECT OrderID, ShippedDate, ShipVia
+FROM Orders
+WHERE ShippedDate >= '1998/05/06' AND ShipVia = 2
