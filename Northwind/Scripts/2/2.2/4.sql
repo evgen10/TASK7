d91c34e5bb609900bbaab20dffa1a3d244a84bcf
evgen10 <i.am.mr.jackson96@gmail.com>
2018-09-03 13:46:36 +0600

    ended

--- Northwind/Scripts/2/2.2/4.sql
+++ Northwind/Scripts/2/2.2/4.sql
@@ -3,10 +3,25 @@
 --то информация о таких покупателя и продавцах не должна попадать в результирующий набор. Не использовать конструкцию JOIN. 
 
 
-SELECT c.City AS [Customer's city], e.City AS [Employee's city], c.CustomerID, e.EmployeeID
-FROM  Employees AS e, Customers AS c
-WHERE e.City = c.City 
 
+SELECT c.City,
+	   STUFF((SELECT DISTINCT ',' + cus.CompanyName
+			  FROM Customers AS cus, Employees AS em
+			  WHERE cus.City = c.City
+			  FOR XML PATH('')), 1,1,''),
+		STUFF((SELECT DISTINCT ',' + em.FirstName+' '+em.LastName
+			  FROM Customers AS cus, Employees AS em
+			  WHERE em.City = e.City
+			  FOR XML PATH('')), 1,1,'')
+FROM Customers AS c,Employees AS e
+WHERE c.City = e.City
+GROUP BY c.City, e.City
+
+
+--SELECT c.City, e.City, c.ContactName, e.FirstName +' '+ e.LastName as [Emloyee name]
+--FROM Customers as c, Employees as e
+--WHERE c.City = e.City
+--GROUP BY c.City,e.City, c.ContactName, e.FirstName +' '+ e.LastName
 
 
 
--- Northwind/Scripts/2/2.2/5.sql
+++ Northwind/Scripts/2/2.2/5.sql
@@ -1,7 +1,11 @@
 ﻿--5. Найти всех покупателей, которые живут в одном городе. 
 
-SELECT c.City, COUNT(c.CustomerID)
-FROM Customers AS c 
-GROUP BY  c.City
+SELECT c.City,
+		STUFF((SELECT ',' + cus.ContactName
+			   FROM Customers cus 
+			   WHERE c.City = cus.City
+			   FOR XML PATH('')),1,1,'') AS Customers
+FROM Customers AS c
+GROUP BY c.City
 
 
